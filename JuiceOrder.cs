6619f95f6a01269f80aed14d81387f7ebd575bba
johnySF <73131199+johnySF@users.noreply.github.com>
2024-02-11 01:42:58 -0300

    Add files via upload

---
+++ JuiceOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABS_Prototype.Models
+{
+    internal class JuiceOrder
+    {
+        private DrinkJuice _drink;
+
+        public bool CreateOrderJuice(DrinkJuice drinkJuice)
+        {
+            if (drinkJuice == null)
+            {
+                Console.Clear();
+                Console.WriteLine("Erro!");
+                return false;
+            }
+            _drink = drinkJuice;
+            Console.WriteLine("Sucesso");
+            return true;
+        }
+    }
+}
---
+++ SodaOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABS_Prototype.Models
+{
+    internal class SodaOrder
+    {
+        private DrinkSoda _drink;
+        
+        public bool CreateOrderSoda(DrinkSoda drinkSoda)
+        {
+            if (drinkSoda == null)
+            {
+                Console.Clear();
+                Console.WriteLine("Erro!");
+                return false;
+            }
+            _drink = drinkSoda;
+            Console.WriteLine("Sucesso");
+            return true;
+        }
+    }
+}
